d20ad4a22030e291012cdfb06db2ee89d2a12546
Laloses <laloses_eduardo@hotmail.com>
2022-02-26 09:13:16 -0600

    Initial Commit

---
+++ ipFraudeAPI/src/main/java/com/ibm/academia/restapi/ipfraude/excepciones/NotFoundException.java
@@ -0,0 +1,12 @@
+package com.ibm.academia.restapi.ipfraude.excepciones;
+
+public class NotFoundException extends RuntimeException{
+
+	public NotFoundException(String mensaje){
+		super(mensaje);
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5839647968836668786L;
+}
