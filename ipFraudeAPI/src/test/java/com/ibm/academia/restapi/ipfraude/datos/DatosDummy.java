470bbe872ec2c839c33871074c612a6deb5c8f92
Laloses <laloses_eduardo@hotmail.com>
2022-02-26 10:38:24 -0600

    remove all  files to apply gitignore

--- ipFraudeAPI/src/test/java/com/ibm/academia/restapi/ipfraude/datos/DatosDummy.java
+++
@@ -1,9 +0,0 @@
-package com.ibm.academia.restapi.ipfraude.datos;
-
-import com.ibm.academia.restapi.ipfraude.modelo.dto.CountryDTO;
-
-public class DatosDummy {
-	public static void country01() {
-		CountryDTO countryDTO = new CountryDTO();
-	}
-}
